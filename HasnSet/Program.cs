393d85acdb5cbdd4a230b864e1ac0a55100a2d6e
JessicaPaola <amysol_18_13@outlook.es>
2026-02-09 17:41:47 -0500

    Primer commit - Proyecto HashSet

---
+++ HasnSet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic; // Esto "importa" los HashSets
+
+// Creamos un HashSet de tipo string (texto)
+var misHobbies = new HashSet<string>();
+
+misHobbies.Add("Piano");
+misHobbies.Add("Cantar");
+misHobbies.Add("Dibujar");
+misHobbies.Add("Piano");
+
+Console.WriteLine($"Cuantos Hobbies hay en total? {misHobbies.Count}");
+
+    if (misHobbies.Contains("Piano"))
+    {
+        Console.WriteLine ("Si Piano , esta en la lista");
+    }
+    
+    else
+    {
+            Console.WriteLine("Nuevo Hobbie por aprender");
+    }
+    
+    
+   
+
+
+
+
